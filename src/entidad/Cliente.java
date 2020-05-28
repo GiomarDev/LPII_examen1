2a281689f02b8baa299989f59c6eca6aaa2c61c3
i201820189 <Giomarlimocaldas1234@gmail.com>
2020-05-27 19:42:43 -0500

    Examen LPII, Giomar Limo Caldas

---
+++ src/entidad/Cliente.java
@@ -0,0 +1,14 @@
+package entidad;
+
+import lombok.Data;
+
+@Data
+public class Cliente {
+private int idCliente;
+private String nombre;
+private String apellido;
+private String corrreo;
+private String fecha;
+private String dni;
+
+}
